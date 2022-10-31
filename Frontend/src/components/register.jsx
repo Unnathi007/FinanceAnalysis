fbd2b46c762a932fd1719a644b0b3f0a0cfd5482
teja-2123 <batrajusaiteja24@gmail.com>
2022-10-31 16:41:29 +0530

    updated

---
+++ Frontend/src/components/register.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "../assets/styles/login.css";
+import NavBar from "./navbar";
+
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardImg
+} from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import { GoogleLoginButton } from "react-social-login-buttons";
+import img from '../assets/images/8432.jpg';
+import Footer from "./footer";
+function RegisterPage() {
+  let navigate = useNavigate();
+  return (<>
+    <NavBar/>
+    <div className="background">
+      <div className="login-box">
+        <div className="container">
+          <div class="row app-des">
+            <div class="col left-background ">
+              <br />
+              <h2>Expense Analysis</h2>
+              <p>We analyze your savings and expenses!</p>
+              <br />
+              <br />
+
+              <CardImg
+                className="mobile-img"
+                src={img}
+                alt="finance"
+              />
+            </div>
+            <div class="col login-form">
+              <form>
+                <br />
+                <h2 className="font-weight-bold mb-4">Register</h2>
+                <FormGroup>
+                  <Label className="font-weight-bold mb-2">Email</Label>
+                  <Input
+                    className="mb-3"
+                    type="email"
+                    placeholder="Enter you Email"
+                  />
+                  <Label className="font-weight-bold mb-2">FirstName</Label>
+                  <Input
+                    className="mb-3"
+                    type="text"
+                    placeholder="Enter First Name"
+                  />
+                  <Label className="font-weight-bold mb-2">LastName</Label>
+                  <Input
+                    className="mb-3"
+                    type="text"
+                    placeholder="Enter Last Name"
+                  />
+                  <Label className="font-weight-bold mb-2">Password</Label>
+                  <Input
+                    className="mb-3"
+                    type="password"
+                    placeholder="At least 8 characters"
+                  />
+                  <Label className="font-weight-bold mb-2">Re-Enter Password</Label>
+                  <Input
+                    className="mb-3"
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                </FormGroup>
+                <Button onClick={() => {
+                    navigate('/user');
+                  }}
+                  className="mt-3  btn">Register to your account</Button>
+                <Button onClick={() => {
+                    navigate('/');
+                  }}
+                  className="mt-3  btn">Already a User SignIn here!</Button>
+                {/* <div className="text-center m-4">or continue with social account</div>
+              <GoogleLoginButton className="mt-3 mb-3 px-auto text-center"/> */}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+    </>
+  );
+}
+
+export default RegisterPage;
---
+++ Frontend/src/components/user.jsx
@@ -0,0 +1,182 @@
+import React from "react";
+import "../assets/styles/login.css";
+import NavBar from "./navbar";
+import Footer from "./footer";
+import Table from "react-bootstrap/Table";
+import "../assets/styles/user.css";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardImg,
+} from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import { GoogleLoginButton } from "react-social-login-buttons";
+
+function UserPage() {
+  return (
+    <React.Fragment>
+      <div className="main-container">
+        <NavBar />
+        <div className="table-container">
+          <div className="expenses">
+            <h1>Expenses</h1>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Harry</td>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <div className="Incomes">
+            <h1>Incomes</h1>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Harry</td>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default UserPage;
